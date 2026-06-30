576bda5c0e19246cb10a9733bf44aa8e24aa4e4a
Pedro Lima <pedro@pedrolima.me>
2026-06-30 11:35:09 +0100

    GMT tag

--- app/ev-charger/layout.tsx
+++ app/ev-charger/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "EV Charger Installation From £879 | Hey Electrics",
@@ -11,5 +12,32 @@ export default function EVChargerLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-ev"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TP7GRGGG');`,
+        }}
+      />
+
+      {/* GTM noscript fallback */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TP7GRGGG"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
+      {children}
+    </>
+  );
 }
